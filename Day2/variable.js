f17d2d5e508880a46e50f67b7c97eb4cf5ecb877
MD-KAIF16 <mdkaif.1608@gmail.com>
2025-12-09 05:31:45 +0000

    Day2 Training Save

---
+++ Day2/variable.js
@@ -0,0 +1,52 @@
+//First programe
+
+
+// let a = 23
+// const b = "abc"
+// var c = null
+//  d = 88;
+
+// console.log("The value of a " ,a);
+// console.log("The value of b " ,b);
+// console.log("The value of c "  , c);
+// console.log("The value of d " , d);
+
+
+
+
+
+//Second programme 
+
+// let a = 23
+// let b = " "
+// let c = null
+// let d ;
+
+// console.log("The value of a " ,a);
+// console.log("The value of b " ,b);
+// console.log("The value of c "  , c);
+// console.log("The value of d " , d);
+
+// console.log("All Variable types are as follows ");
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+
+
+
+
+
+//third task
+
+let score = "33absbc";
+console.log(typeof(score));
+console.log(typeof score);
+
+//type cast
+let New = Number(score);
+
+console.log(typeof(New));
+console.log(typeof New);
+
+console.log(New);
